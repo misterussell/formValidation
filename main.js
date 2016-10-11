12afa6e837810b0cb69f465ed63da48823a28b38
Maxwell Russell <maxruss.87@gmail.com>
2016-10-11 18:07:22 -0500

    added in form completion behavoir

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var completedForm = {};
 // ARRAY FOR CLASS
 var formFields = ['.firstName' , '.lastName' , '.email' , '.country' , '.bio' , '.phone' , '.affiliations' , '.occupation' , '.catName' , '.favGadget' , '.talent' , '.favDrink' , '.specialPower' , '.weapon' , '.comments'];
 
+var incompleteFields = {};
 
 // FOR COMPAIRING BY ID
 // submitButton.addEventListener('click', function() {
@@ -22,18 +23,31 @@ var formFields = ['.firstName' , '.lastName' , '.email' , '.country' , '.bio' , 
 //   console.log(completedForm);
 // });
 
+function setColor(element) {
+  element.style.backgroundColor = '#86A344';
+}
+
 submitButton.addEventListener('click', function() {
+  var formCompletion = true;
   for(x = 0; x < formFields.length; x++) {
     if (document.querySelector(formFields[x]).value === '') {
-      alert('You have not finished submitting information.');
-      // console.log(document.querySelector(formFields[x]).value);
-      x = formFields.length + 1;
+      incompleteFields[x] = document.querySelector(formFields[x]).name;
+      formCompletion = false;
+      setColor(document.querySelector(formFields[x]));
     } else {
       // completedForm[formFields[0]] = firstName.value;
       // x = x + 1;
       completedForm[formFields[x]] = document.querySelector(formFields[x]).value;
     }
   }
-  console.log("Thank you for submitting.");
-  console.log(completedForm);
+  if (formCompletion) {
+    alert("Thank you for submitting. We will be in touch!");
+    console.log(completedForm);
+  } else {
+    console.log('You have not finished submitting information. Please fill out the fields highlighted in green.');
+    // CONSOLE all values in the incomplete field
+    // for (var y in incompleteFields) {
+    //   console.log(incompleteFields[y]);
+    // }
+  }
 });
